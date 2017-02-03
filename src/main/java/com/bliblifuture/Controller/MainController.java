6943bead57ba900e8eb809b6ff4d117f1c9e6608
Fransiskus A Kristiawan <frans_kristiawan@yahoo.com>
2017-02-03 12:47:56 +0700

    Initial commit saat di depan ruang e5, html sudah masuk dan hasil compile OK, perlu benahi cssnya

---
+++ src/main/java/com/bliblifuture/Controller/MainController.java
@@ -0,0 +1,7 @@
+package com.bliblifuture.Controller;
+
+/**
+ * Created by My Windows on 2/3/2017.
+ */
+public class MainController {
+}
---
+++ src/main/java/com/bliblifuture/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.bliblifuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class DemoApplication extends WebMvcConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**")
+				.addResourceLocations("classpath:/static/");
+	}
+}
