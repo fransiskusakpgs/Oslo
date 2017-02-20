25c09876a978ecafcc86922ea1217e0f8cf35a01
Fransiskus A Kristiawan <frans_kristiawan@yahoo.com>
2017-02-20 11:21:08 +0700

    commit saat css udah masuuuk, image masuk,

--- src/main/java/com/bliblifuture/Controller/MainController.java
+++ src/main/java/com/bliblifuture/Controller/MainController.java
@@ -1,7 +1,19 @@
 package com.bliblifuture.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by My Windows on 2/3/2017.
  */
+
+@Controller
+
 public class MainController {
+
+    @RequestMapping("/")
+    private String lalala() {
+        return "index";
+    }
+
 }
