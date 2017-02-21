a2be150103340b93363edd3c8a7cf67597e83cd2
fransiskusak <f.a.k@mail.ugm.ac.id>
2017-02-21 22:39:54 +0700

    commit saat mencoba stash

--- src/main/java/com/bliblifuture/Controller/MainController.java
+++ src/main/java/com/bliblifuture/Controller/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by My Windows on 2/3/2017.
+ * Created by ASUS My Windows on 2/3/2017.
  */
 
 @Controller
