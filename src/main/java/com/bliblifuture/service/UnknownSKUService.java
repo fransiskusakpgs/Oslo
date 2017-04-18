a95629363d16bb02b9a11164589f9996d51d4e4e
fransiskusak <f.a.k@mail.ugm.ac.id>
2017-04-18 15:19:15 +0700

    commit di dunkin: membuat http get buat unknown skus, sementara sudah kepanggil hasil jsonnya. merubah method add ke set

--- src/main/java/com/bliblifuture/service/UnknownSKUService.java
+++ src/main/java/com/bliblifuture/service/UnknownSKUService.java
@@ -1,13 +1,17 @@
 package com.bliblifuture.service;
 
 import com.bliblifuture.model.SKU;
+import com.bliblifuture.model.StockOpname;
 import com.bliblifuture.model.UnknownSKU;
 import com.bliblifuture.repository.SKURepository;
+import com.bliblifuture.repository.StockOpnameRepository;
 import com.bliblifuture.repository.UnknownSKURepository;
+import com.bliblifuture.request.SingleRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.StringTokenizer;
 
 @Service
 public class UnknownSKUService {
@@ -15,13 +19,25 @@ public class UnknownSKUService {
     SKURepository SKUrepo;
     @Autowired
     UnknownSKURepository UnknownSKUrepo;
+    @Autowired
+    StockOpnameRepository stockOpnameRepo;
 
-    public List<SKU> findAll(){
-        List<SKU> data11 = SKUrepo.findAll();
-        return data11;
-    }
-    public List<UnknownSKU> findAll2(){
-        List<UnknownSKU> data12 = UnknownSKUrepo.findAll();
+
+
+
+//    public List<UnknownSKU> findUnknownSKUByStockOpname(SingleRequest request){ //data request udah dipassing disini
+//
+//        StockOpname a = stockOpnameRepo.findByStockOpnameId(request.getData());
+//
+//        List<UnknownSKU> data12 = UnknownSKUrepo.findByStockOpname(a);
+//
+//        return data12;
+//    }
+
+    public List<UnknownSKU> findUnknownSKUByStockOpname(){ //data request udah dipassing disini
+        String activeStockOpname = "100"; //ini cuma duummy Stockopname ID
+        StockOpname a = stockOpnameRepo.findByStockOpnameId(activeStockOpname);
+        List<UnknownSKU> data12 = UnknownSKUrepo.findByStockOpname(a);
         return data12;
     }
 
