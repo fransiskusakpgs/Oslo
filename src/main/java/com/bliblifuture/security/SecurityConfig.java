802f33fb708cfa9934e4344f4ab4152b7966ea7d
Elisabeth Diana Kartika S <elisabeth.dianaks@gmail.com>
2017-04-23 10:26:02 +0700

    Spring Security Basic Installation

---
+++ src/main/java/com/bliblifuture/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.bliblifuture.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+                httpBasic().and().
+                csrf().disable();
+    }
+}
---
+++ src/main/java/com/bliblifuture/security/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.bliblifuture.security;
+
+import com.bliblifuture.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+class SecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    @Autowired
+    UserRepository userRepo;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                com.bliblifuture.model.User user = userRepo.findByUsername(username);
+                if(user != null) {
+                    return new User(user.getUsername(), user.getPassword(), true, true, true, true,
+                            AuthorityUtils.createAuthorityList("USER"));
+                } else {
+                    throw new UsernameNotFoundException("could not find the user '"
+                            + username + "'");
+                }
+            }
+
+        };
+    }
+}
